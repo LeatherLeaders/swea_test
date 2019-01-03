1215ef3119214fadc89dce96e47a46bafe4df4e0
이상윤 <sangyun4073@gmail.com>
2019-01-03 10:13:06 +0900

    finish 4831:

--- SWtest_code/swea_4831.py
+++ SWtest_code/swea_4831.py
@@ -1,19 +1,27 @@
 T = int(input())
 # x=시작점
-def next_stop(stop,k,n,m):
-    cnt = 0
-    for j in range(stop+k, stop-1, -1):
-        if (j in elec):
-            stop = j
-            cnt +=1
-        if(j+k>n):
+def next_stop(stop,cnt):
+
+    for i in range(stop + k, stop, -1):
+        if (i in elec):
+            stop = i
+            cnt += 1
+            break
+        elif(i==stop+1):
+            cnt=-1
+    if (cnt == -1):
+        print("#" + str(j + 1), 0)
+    elif(stop+k>=n) :
+        print("#" + str(j + 1), cnt)
+    else:
+        next_stop(stop,cnt)
 
 
 
-for i in range(T) :
+
+for j in range(T) :
     k,n,m = map(int,input().split())
     elec = list(map(int,input().split()))
-    cnt=0
-    stop=0
 
-    print("#" + str(i + 1), cnt)
+    next_stop(0,0)
+    # print("#" + str(i + 1), count)
