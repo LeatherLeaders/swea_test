9af34d8e044afdce62147174b2e8808e3ad8e279
이상윤 <sangyun4073@gmail.com>
2019-01-05 11:45:52 +0900

    swea4836_finish

--- SWtest_code/swea_4836.py
+++ SWtest_code/swea_4836.py
@@ -1,20 +1,27 @@
 T = int(input())
 # [[x좌표,y좌표,색칠된 컬러(1,2)]] --> len=4 이면 count한다
 for i in range(T) :
+
     N = int(input())
-    L=[[i,j] for i in range(10) for j in range(10)]
+    R_1=[]
+    R_2=[]
+
     for j in range(N) :
         x1, y1, x2, y2, c1 = map(int, input().split())
-        R=[[i,j] for i in range(x1,x2+1) for j in range(y1,y2+1)]
-        for x in L:
-            for y in R:
-                if (x[:2] == y):
-                    x.append(c1)
-    cnt = 0
-    for x in L :
-        for y in R :
-            if(x[:2]==y and len(x)==4):
-                cnt+=1
+        if(c1==1):
+            R = [[i, j] for i in range(x1, x2 + 1) for j in range(y1, y2 + 1)]
+            for k in R :
+                if(not k in R_1):
+                    R_1.append(k)
+        else :
+            R = [[i, j] for i in range(x1, x2 + 1) for j in range(y1, y2 + 1)]
+            for k in R :
+                if(not k in R_2):
+                    R_2.append(k)
 
+    cnt = 0
+    for x in R_1 :
+        if(x in R_2):
+            cnt+=1
 
     print("#" + str(i + 1),cnt)
