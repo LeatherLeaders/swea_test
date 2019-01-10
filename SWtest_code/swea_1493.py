33a6ef4ee89f7680f8190c0298d37c966e9e9214
이상윤 <sangyun4073@gmail.com>
2019-01-10 14:09:58 +0900

    swea_1493 check test case

--- SWtest_code/swea_1493.py
+++ SWtest_code/swea_1493.py
@@ -1,6 +1,6 @@
 T = int(input())
 L=[]
-for j in range(1,201):
+for j in range(1,288):
      for x in range(1,j+1):
          y=-x+j+1
          z=int(j*(j-1)/2+x)
@@ -10,10 +10,16 @@ for i in range(T) :
     m,n = map(int,input().split())
     x_1,y_1,x_2,y_2 = 0,0,0,0
     for j in L :
-        if(j[2]==m):
-            x_1,y_1=j[0],j[1]
-        elif(j[2]==n):
-            x_2,y_2=j[0],j[1]
+        print(j)
+        if(m==n) :
+            if(j[2]==m):
+                x_1,y_1,x_2,y_2=j[0],j[1],j[0],j[1]
+        else :
+            if (j[2] == m):
+                x_1, y_1 = j[0], j[1]
+            elif (j[2] == n):
+                x_2, y_2 = j[0], j[1]
+
     x=x_1+x_2
     y=y_1+y_2
 
